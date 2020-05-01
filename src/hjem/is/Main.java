d43b4d3286af46b8a95e8f2be665ed6957188e03
Alexandra Østermark <alex.cramt@gmail.com>
2020-05-01 11:44:50 +0200

    excel works

--- src/hjem/is/Main.java
+++ src/hjem/is/Main.java
@@ -7,31 +7,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main {
-    public static void main(String[] args) {
-        Workbook wb = new XSSFWorkbook();
-        try (OutputStream fileOut = new FileOutputStream("workbook.xlsx")) {
-            wb.write(fileOut);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        /*
-        double[] x = new double[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-        double[] y = new double[]{0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361};
-        TrendLine line = OLSTrendLine.assumeTrend(y, x);
-        System.out.println(line.toString());
-        System.out.println(line.predict(5.5));
-        */
-        /*
+    private static void regressionExample() {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
@@ -43,6 +26,32 @@ public class Main {
                 System.out.print(integer + ", ");
             }
         }
-         */
+    }
+
+    private static void excelExample() {
+        //https://poi.apache.org/components/spreadsheet/quick-guide.html#NewWorkbook
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet = wb.createSheet("new sheet");
+        CreationHelper createHelper = wb.getCreationHelper();
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+        cell.setCellValue(1);
+        cell = row.createCell(1);
+        cell.setCellValue(1.2);
+        cell = row.createCell(2);
+        cell.setCellValue(createHelper.createRichTextString("string gang"));
+        cell = row.createCell(3);
+        cell.setCellValue(true);
+        try (OutputStream fileOut = new FileOutputStream("workbook.xlsx")) {
+            wb.write(fileOut);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        excelExample();
     }
 }
