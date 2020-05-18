c33165fafc3dccf23d2f4b7c53eb00265e81fb61
Alexandra Østermark <alex.cramt@gmail.com>
2020-05-18 10:07:27 +0200

    added interfaces for the data stores

--- src/hjem/is/db/IPeriodicPlanStore.java
+++ src/hjem/is/db/IPeriodicPlanStore.java
@@ -1,4 +1,12 @@
 package hjem.is.db;
 
+import hjem.is.model.PeriodicPlan;
+import hjem.is.model.StoragePlan;
+
+import java.util.List;
+
 public interface IPeriodicPlanStore {
+    PeriodicPlan getById(int id) throws DataAccessException;
+    List<PeriodicPlan> getByStoragePlan(StoragePlan storagePlan) throws DataAccessException;
+    void add(PeriodicPlan periodicPlan) throws DataAccessException;
 }
--- src/hjem/is/db/IStoragePlanStore.java
+++ src/hjem/is/db/IStoragePlanStore.java
@@ -1,4 +1,12 @@
 package hjem.is.db;
 
+import hjem.is.model.StoragePlan;
+
+import java.util.List;
+
 public interface IStoragePlanStore {
+    StoragePlan getById(int id) throws DataAccessException;
+    StoragePlan getByName(String name) throws DataAccessException;
+    void add(StoragePlan storagePlan) throws DataAccessException;
+    List<StoragePlan> getALl() throws DataAccessException;
 }
