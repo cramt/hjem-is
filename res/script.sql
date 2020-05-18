153ede5540c6e797ff1334ec804fbcbccaf3cada
Alexandra Østermark <alex.cramt@gmail.com>
2020-05-18 09:40:16 +0200

    fixed errors in sql script

--- res/script.sql
+++ res/script.sql
@@ -41,7 +41,7 @@ CREATE TABLE storage_orders
     seasonal_plan_id INT NOT NULL,
     supplier_id      INT NOT NULL,
     primary key (id),
-    foreign key (seasonal_plan_id) references seasonal_plans (id) on delete cascade,
+    foreign key (seasonal_plan_id) references periodic_plans (id) on delete cascade,
     foreign key (supplier_id) references suppliers (id) on delete cascade
 )
 
@@ -58,9 +58,10 @@ CREATE TABLE product_lines
     id               INT identity (1,1),
     amount           INT NOT NULL,
     product_id       INT NOT NULL,
-    storage_order_id INT NOT NULL,
+    storage_order_id INT,
     primary key (id),
-    foreign key (product_id) references products (id) on delete cascade
+    foreign key (product_id) references products (id) on delete cascade,
+    foreign key (storage_order_id) references storage_orders (id) on delete cascade
 )
 
 CREATE TABLE products_supplier
@@ -75,7 +76,7 @@ CREATE TABLE periodic_plans_products_map
 (
     product_id       INT NOT NULL,
     amount           INT NOT NULL,
-    seasonal_plan_id INT NOT NULL,
+    periodic_plan_id INT NOT NULL,
     foreign key (product_id) references products (id) on delete cascade,
-    foreign key (seasonal_plan_id) references seasonal_plans (id) on delete cascade
+    foreign key (periodic_plan_id) references periodic_plans (id) on delete cascade
 )
---
+++ src/hjem/is/db/ResultSetWrapper.java
@@ -0,0 +1,4 @@
+package hjem.is.db;
+
+public class ResultSetWrapper {
+}
