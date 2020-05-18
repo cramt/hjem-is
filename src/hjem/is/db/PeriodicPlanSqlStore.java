170a8355c31a5bf0ec02c5e5827eb34237dcbccb
Alexandra Østermark <alex.cramt@gmail.com>
2020-05-18 10:08:16 +0200

    implemented stubbs for implementations

--- src/hjem/is/db/PeriodicPlanSqlStore.java
+++ src/hjem/is/db/PeriodicPlanSqlStore.java
@@ -1,5 +1,24 @@
 package hjem.is.db;
 
+import hjem.is.model.PeriodicPlan;
+import hjem.is.model.StoragePlan;
+
+import java.util.List;
+
 public class PeriodicPlanSqlStore implements IPeriodicPlanStore {
 
+    @Override
+    public PeriodicPlan getById(int id) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public List<PeriodicPlan> getByStoragePlan(StoragePlan storagePlan) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void add(PeriodicPlan periodicPlan) throws DataAccessException {
+
+    }
 }
--- src/hjem/is/db/StoragePlanSqlStore.java
+++ src/hjem/is/db/StoragePlanSqlStore.java
@@ -1,5 +1,28 @@
 package hjem.is.db;
 
+import hjem.is.model.StoragePlan;
+
+import java.util.List;
+
 public class StoragePlanSqlStore implements IStoragePlanStore{
 
+    @Override
+    public StoragePlan getById(int id) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public StoragePlan getByName(String name) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void add(StoragePlan storagePlan) throws DataAccessException {
+
+    }
+
+    @Override
+    public List<StoragePlan> getALl() throws DataAccessException {
+        return null;
+    }
 }
