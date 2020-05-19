b3d1f6c288d1900040879e43d0929905128667e1
penismusic <1080615@ucn.dk>
2020-05-19 10:17:53 +0200

    all getters and setters for controllers

--- src/hjem/is/control/ProductController.java
+++ src/hjem/is/control/ProductController.java
@@ -1,5 +1,40 @@
 package hjem.is.control;
 
-public class ProductController {
+import hjem.is.model.Product;
+import hjem.is.model.Supplier;
 
+public class ProductController {
+    private Product product;
+
+    public ProductController(Product product) {
+        this.product = product;
+    }
+
+    public Integer getId() {
+        return product.getId();
+    }
+
+    public int getCost() {
+        return product.getCost();
+    }
+
+    public void setCost(int cost) {
+        product.setCost(cost);
+    }
+
+    public String getName() {
+        return product.getName();
+    }
+
+    public void setName(String name) {
+        product.setName();
+    }
+
+    public Supplier getSupplier() {
+        return product.getSupplier();
+    }
+
+    public void setSupplier(Supplier supplier) {
+        product.setSupplier(supplier);
+    }
 }
