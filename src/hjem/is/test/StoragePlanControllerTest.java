f41492527134c90c2dda62feb487afabd73b46da
Lucca Christiansen <rkaasgaard@gmail.com>
2020-05-23 16:21:32 +0200

    more testing

--- src/hjem/is/test/StoragePlanControllerTest.java
+++ src/hjem/is/test/StoragePlanControllerTest.java
@@ -1,6 +1,8 @@
 package hjem.is.test;
 
+import hjem.is.controller.PeriodicPlanController;
 import hjem.is.controller.StoragePlanController;
+import hjem.is.controller.regression.ModelFinder;
 import hjem.is.db.DataAccessException;
 import hjem.is.db.IStoragePlanStore;
 import hjem.is.db.StoragePlanSqlStore;
@@ -13,30 +15,31 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class StoragePlanControllerTest {
 
-    @Mock
-    IStoragePlanStore spsMock;
+    @Mock IStoragePlanStore spsMock;
+    @Mock PeriodicPlanController ppcMock;
+    @Mock ModelFinder mfMock;
 
-    @InjectMocks
-    StoragePlanController spc = new StoragePlanController();
+    @InjectMocks StoragePlanController spc = new StoragePlanController();
 
     @Test
     public void generatesNewPlan() {
-
+        fail("Not yet implemented");
     }
 
     @Test
     public void getsPeriodicPlanControllerWithIndex() {
-
+        fail("Not yet implemented");
     }
 
     @Test
     public void tellsDatabaseToSaveNewStoragePlan() {
-        StoragePlan sp = new StoragePlan("June", false,new StorageMetaData(100f), new ArrayList<PeriodicPlan>());
+        StoragePlan sp = new StoragePlan("June", false, new StorageMetaData(100f), new ArrayList<PeriodicPlan>());
         try {
             when(spsMock.getByName("June")).thenReturn(sp);
         } catch (DataAccessException ignored) {
@@ -53,12 +56,25 @@ public class StoragePlanControllerTest {
     }
 
     @Test
-    public void tellsDataBaseToSaveExistingStoragePlan() {
+    public void tellsDataBaseToUpdateExistingStoragePlan() {
+        StoragePlan sp = new StoragePlan("July", false, new StorageMetaData(100f), new ArrayList<PeriodicPlan>(), 3);
+        try {
+            when(spsMock.getByName("July")).thenReturn(sp);
+        } catch (DataAccessException ignored) {
+        }
+        spc.select("July");
+        spc.save();
 
+        try {
+            verify(spsMock, times(1)).update(sp);
+            verify(spsMock, never()).add(sp);
+        } catch (DataAccessException ignored) {
+        }
     }
 
     @AfterEach
     public void tearDown() {
         reset(spsMock);
+        reset(ppcMock);
     }
 }
